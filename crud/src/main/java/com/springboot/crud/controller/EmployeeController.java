beaf9b8e9c81f4f57e5673542624691a4f364b31
p.sumara <sumara0498@icloud.com>
2020-09-29 10:10:54 +0200

    Added support for adding, updating and deliting customers

--- crud/src/main/java/com/springboot/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/springboot/crud/controller/EmployeeController.java
@@ -4,11 +4,8 @@ import com.springboot.crud.entity.Employee;
 import com.springboot.crud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -28,7 +25,43 @@ public class EmployeeController {
 
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		Employee theEmployee = new Employee();
+
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId")int theId, Model theModel) {
+
+		Employee theEmployee = employeeService.findById(theId);
+
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId){
+
+		employeeService.deleteById(theId);
+
+		return "redirect:/employees/list";
 	}
 }
 
--- crud/src/main/java/com/springboot/crud/dao/EmployeeRepository.java
+++ crud/src/main/java/com/springboot/crud/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.springboot.crud.dao;
 import com.springboot.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
