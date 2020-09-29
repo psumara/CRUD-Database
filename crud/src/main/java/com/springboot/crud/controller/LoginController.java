a79e01e9f150205815fadbc2a6986b96167834f5
p.sumara <sumara0498@icloud.com>
2020-09-29 12:59:28 +0200

    Added support for login page with users roles

---
+++ crud/src/main/java/com/springboot/crud/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.springboot.crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+
+        return "access-denied";
+    }
+}
